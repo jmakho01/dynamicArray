5b02de6fb3602a21d04f85c36877a86e5d9a5ebb
Omnissiah <234093135+Zeta1313@users.noreply.github.com>
2026-01-08 14:37:11 -0800

    Created testing shell

---
+++ DSLTests.java
@@ -0,0 +1,33 @@
+import org.junit.Test;
+
+public class DSLTests {
+    @Test
+    void testCreate() {
+
+    }
+
+    @Test
+    void testAdd() {
+
+    }
+
+    @Test
+    void testGet() {
+
+    }
+
+    @Test
+    void testSize() {
+
+    }
+
+    @Test 
+    void testRemove() {
+
+    }
+
+    @Test
+    void testCapacity() {
+        
+    }
+}
